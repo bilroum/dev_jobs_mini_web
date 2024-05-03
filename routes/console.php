ca051621ffb6c73fe1243f1e0d284945a554e535
bil <bilroum@gmail.com>
2024-05-03 13:18:46 +0200

    //automate the process of deleting expired tokens

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+//automate the process of deleting expired tokens
+Schedule::command('auth:clear-resets')->everyFifteenMinutes();
